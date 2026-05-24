60969d87ccc9e03bc109479381971d9322d62e1d
Doug Green <douglasrogergreen@gmail.com>
2026-05-23 18:08:59 -0800

    Initial Commit

---
+++ TrueNAS Remote/TrueNAS_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueNAS_RemoteApp.swift
+//  TrueNAS Remote
+//
+//  Created by Doug Green on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TrueNAS_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
